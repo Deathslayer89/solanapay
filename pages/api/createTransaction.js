cb52e30b221eaf7f5728a475619ce86adcf6d4a6
Deathslayer89 <Dineshswain2001@gmail.com>
2022-06-03 14:40:33 +0530

    shifting to mainnet

--- pages/api/createTransaction.js
+++ pages/api/createTransaction.js
@@ -4,9 +4,10 @@ import { createTransferCheckedInstruction, getAssociatedTokenAddress, getMint } 
 import BigNumber from "bignumber.js";
 import products from "./products.json";
 
-const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
-const sellerAddress ="HasupRr12BNvpnPG4eoadio9sJRKbF9nRMJx5gC7oueM";
-const sellerPublicKey = new PublicKey(sellerAddress);
+// const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
+const usdcAddress = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+
+const sellerPublicKey = new PublicKey(process.env.NEXT_PUBLIC_SELLER_PUBLIC_KEY);
 
 const createTransaction = async (req, res) => {
   try {
